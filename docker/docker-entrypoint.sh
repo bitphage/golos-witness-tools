1887ded59eac344f5675fc49ed47eeafe66865a4
Vladimir Kamarzin <vvk@vvk.pp.ru>
2017-10-18 22:52:45 +0500

    Add docker-entrypoint.sh

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+render_templates()
+{
+    confd -onetime -backend env
+}
+
+# default settings
+export node="${NODE:-wss://api.golos.cf}"
+export witness="${WITNESS:-foo}"
+export key="${KEY:-key}"
+export interval="${INTERVAL:-3600}"
+export max_age="${MAX_AGE:-86400}"
+export threshold="${THRESHOLD:-0.15}"
+
+render_templates
+exec "$@"
