553608fb51ce1743d6e3cee8bceb7f8ce3e6c244
Vladimir Kamarzin <vvk@vvk.pp.ru>
2018-04-02 12:45:58 +0500

    update_price_feed.py: remove --version arg

--- update_price_feed.py
+++ update_price_feed.py
@@ -54,7 +54,6 @@ def main():
     parser = argparse.ArgumentParser(
             description='golos price feed updater',
             epilog='Report bugs to https://github.com/bitfag/golos-witness-tools/issues')
-    parser.add_argument('-v', '--version', action='version', version='%(prog)s 1710.3')
     parser.add_argument('-c', '--config', default='./update_price_feed.yml',
             help='specify custom path for config file')
     parser.add_argument('-m', '--monitor', action='store_true',
