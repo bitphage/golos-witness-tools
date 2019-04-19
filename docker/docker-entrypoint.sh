c99b52f584cec547133f333c2e97d27607eb0196
Vladimir Kamarzin <vvk@vvk.pp.ru>
2019-04-19 18:59:24 +0500

    Update docker files

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -11,9 +11,26 @@ export witness="${WITNESS:-foo}"
 export key="${KEY:-key}"
 
 # settings for update_price_feed.py
+export price_source="${SOURCE:-bitshares}"
+export node_bts="${NODE_BTS:-"
+  - wss://eu.nodes.bitshares.ws
+  - wss://bitshares.openledger.info/ws
+  - wss://citadel.li/node
+  - wss://api-ru.bts.blckchnd.com
+  - wss://api.bts.blckchnd.com"
+}"
+export markets="${MARKETS:-"
+  - RUDEX.GOLOS/BTS
+  - RUDEX.GOLOS/RUDEX.BTC
+  - RUDEX.GOLOS/RUBLE
+  - RUDEX.GOLOS/USD
+  - RUDEX.GOLOS/CNY"
+}"
+export metric="${METRIC:-weighted_average}"
+export depth_pct="${DEPTH_PCT:-20}"
 export interval="${INTERVAL:-3600}"
 export max_age="${MAX_AGE:-86400}"
-export threshold="${THRESHOLD:-0.05}"
+export threshold_pct="${THRESHOLD_PCT:-10}"
 export k="${K:-1}"
 
 # settings for witness_monitor.py
