627769cdb482d3b6c6fd9802f5e2f34270dcb07b
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-05-06 00:07:58 +0500

    Fully refactor remaining scripts

--- update_price_feed.py
+++ update_price_feed.py
@@ -1,140 +1,14 @@
 #!/usr/bin/env python
 
-# This script is inspired by roelandp script https://github.com/roelandp/golospricefeed
-
-import sys
 import argparse
+import asyncio
 import logging
+import sys
+
 import yaml
-import statistics
-import time
+from golosscripts.feed import FeedUpdater
 
-from golos import Steem
-from bitshares_helper import BitSharesHelper
-
-from functions import (
-    get_price_gold,
-    get_price_usd_btc,
-    get_price_btc_golos,
-    get_price_btc_gbg,
-    split_pair,
-    calc_weighted_average_price,
-    get_witness,
-    get_old_price,
-    get_median_price,
-    last_price_too_old,
-    publish_price,
-)
-
-log = logging.getLogger('functions')
-
-
-def calculate_gbg_golos_price_cmc():
-    """ Old method to calculate price using coinmarketcap
-    """
-
-    # market price USD/gold
-    price_mg_gold = get_price_gold()
-    log.info('USD/gold: {:.4f}'.format(price_mg_gold))
-
-    # market price USD/BTC
-    usd_btc = get_price_usd_btc()
-    log.info('USD/BTC: {:.0f}'.format(usd_btc))
-
-    # market price BTC/GOLOS
-    btc_golos = get_price_btc_golos()
-    log.info('BTC/GOLOS: {:.8f}'.format(btc_golos))
-
-    # market price BTC/GBG just for analytics
-    price_btc_gbg = get_price_btc_gbg()
-    log.info('BTC/GBG: {:.8f}'.format(price_btc_gbg))
-
-    # calculated price BTC/gold just for analytics
-    price_btc_gold = price_mg_gold / usd_btc
-    log.info('BTC/gold: {:.8f}'.format(price_btc_gold))
-
-    # BTC/GBG / BTC/gold rate just for analytics
-    gbg_gold_rate = price_btc_gbg / price_btc_gold
-    log.info('GBG/gold rate: {:.2f}'.format(gbg_gold_rate))
-
-    # calculated price USD/golos just for analytics
-    price_usd_golos = btc_golos * usd_btc
-    log.info('USD/GOLOS: {:.2f}'.format(price_usd_golos))
-
-    # how many mgs of gold in 1 GOLOS == GOLOS/GBG (1 GBG == 1 mg gold)
-    price_gold_golos = price_usd_golos / price_mg_gold
-    log.info('GBG/GOLOS: {:.3f}'.format(price_gold_golos))
-
-    return price_gold_golos
-
-
-def calculate_gbg_golos_price_bts(bitshares, markets, metric='median', depth_pct=20):
-    """ Calculate price GBG/GOLOS
-
-        :param BitSharesHelper bitshares:
-        :param list markets: list of markets to use to obtain price, in format ['QUOTE/BASE']
-        :param str metric: what metric to use to calculate price
-        :param float depth_pct: how deeply measure market for volume
-        :return: price
-        :rtype: float
-    """
-
-    prices = []
-    price_bts_golos = 0
-
-    assert markets is not None
-
-    for market in markets:
-        quote, base = split_pair(market)
-        # Price and volume on GOLOS/XXX market
-        price1, volume1 = bitshares.get_market_center_price(market, depth_pct=20)
-        log.info('Raw price {:.8f} {}/{}'.format(price1, base, quote))
-
-        # Price and volume on XXX/BTS market
-        if base != 'BTS':
-            tmp_market = '{}/BTS'.format(base)
-            price2, volume2 = bitshares.get_market_center_price(tmp_market, depth_pct=20)
-            price = price1 * price2
-            # Limit volume by smallest volume across steps
-            try:
-                volume = min(volume1, volume2 / price1)
-            except ZeroDivisionError:
-                volume = 0
-        else:
-            price, volume = price1, volume1
-
-        log.debug('Derived price BTS/GOLOS from market {}: {:.8f}, volume: {:.0f}'.format(market, price, volume))
-        prices.append({'price': price, 'volume': volume, 'market': market})
-
-    price_list = [v for element in prices for k, v in element.items() if k == 'price']
-    price_bts_golos_median = statistics.median(price_list)
-    log.info('Median market price: {:.8f} BTS/GOLOS'.format(price_bts_golos_median))
-    price_bts_golos_mean = statistics.mean(price_list)
-    log.info('Mean market price: {:.8f} BTS/GOLOS'.format(price_bts_golos_mean))
-    price_bts_golos_wa = calc_weighted_average_price(prices)
-    log.info('Weighted average market price: {:.8f} BTS/GOLOS'.format(price_bts_golos_wa))
-
-    if metric == 'median':
-        price_bts_golos = price_bts_golos_median
-    elif metric == 'mean':
-        price_bts_golos = price_bts_golos_mean
-    elif metric == 'weighted_average':
-        price_bts_golos = price_bts_golos_wa
-    else:
-        log.critical('unsupported metric')
-        sys.exit(1)
-
-    # 1 GOLD is 1 troy ounce; calc xxx GOLD for 1 BTS
-    price_troyounce = bitshares.get_feed_price('GOLD')
-    gram_in_troyounce = 31.1034768
-    # How many 1mg gold for 1 BTS
-    price_gold_bts = price_troyounce * gram_in_troyounce * 1000
-    log.debug('Gold price is: {:.8f} 1mgGOLD/BTS'.format(price_gold_bts))
-
-    price_gold_golos = price_bts_golos * price_gold_bts
-    log.info('Calculated price {:.3f} GBG/GOLOS'.format(price_gold_golos))
-
-    return price_gold_golos
+log = logging.getLogger('golosscripts')
 
 
 def main():
@@ -180,73 +54,15 @@ def main():
     with open(args.config, 'r') as ymlfile:
         conf = yaml.safe_load(ymlfile)
 
-    # defaults
-    price_source = conf.get('source', 'bitshares')
-    metric = conf.get('metric', 'median')
-    depth_pct = conf.get('depth_pct', 20)
-    threshold_pct = conf.get('threshold_pct', 10) / 100
+    feed_updater = FeedUpdater(**conf)
 
-    # initialize steem instance
-    golos = Steem(nodes=conf['node'], keys=conf['keys'])
+    if args.monitor:
+        asyncio.run(feed_updater.run_forever())
+    else:
+        asyncio.run(feed_updater.publish_price())
 
-    if price_source == 'bitshares':
-        bitshares = BitSharesHelper(node=conf['node_bts'])
-
-    # main loop
-    while True:
-        # flag variable which determine should we actually update price or not
-        need_publish = False
-
-        try:
-            # request current witness info
-            witness_data = get_witness(golos, conf['witness'])
-
-            # calculate prices
-            if price_source == 'bitshares':
-                price = calculate_gbg_golos_price_bts(bitshares, conf['markets'], metric=metric, depth_pct=depth_pct)
-            elif price_source == 'cmc':
-                price = calculate_gbg_golos_price_cmc()
-
-            old_price = get_old_price(witness_data)
-            median_price = get_median_price(golos)
-            log.info('Current median price: {:.3f}'.format(median_price))
-
-            # apply correction if k defined
-            if 'k' in conf:
-                price = price * conf['k']
-                log.info('price after correction: {:.3f}'.format(price))
-
-            # check whether our price is too old
-            last_price_update_too_old = last_price_too_old(witness_data, conf['max_age'])
-            if last_price_update_too_old:
-                log.info('Our last price update older than max_age, forcing update')
-                need_publish = True
-
-            # check for price difference between our old price and new price
-            diff_rel = abs((old_price / price) - 1)
-            if diff_rel > threshold_pct:
-                log.info('publishing price, difference is: {:.2%}'.format(diff_rel))
-                need_publish = True
-            else:
-                log.debug('price difference is too low, not publishing price')
-
-            # finally publish price if needed
-            if need_publish or args.force:
-                if args.dry_run:
-                    log.info('--dry-run mode, not publishing price feed')
-                else:
-                    publish_price(golos, price, account=conf['witness'])
-
-        except Exception:
-            log.exception('exception in main loop:')
-
-        finally:
-
-            if args.monitor:
-                time.sleep(conf['interval'])
-            else:
-                sys.exit(0)
+    return 0
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
--- witness_monitor.py
+++ witness_monitor.py
@@ -2,15 +2,12 @@
 
 import argparse
 import logging
+import sys
+
 import yaml
-import time
-from datetime import datetime
+from golosscripts.monitor import Monitor
 
-from golos import Steem
-
-import functions
-
-log = logging.getLogger('functions')
+log = logging.getLogger('golosscripts')
 
 
 def main():
@@ -37,71 +34,11 @@ def main():
     with open(args.config, 'r') as ymlfile:
         conf = yaml.safe_load(ymlfile)
 
-    golos = Steem(nodes=conf['node'], keys=conf['keys'], num_retries=999999, mode='head')
+    monitor = Monitor(**conf)
+    monitor.run_forever()
 
-    # do not begin monitoring until node will not get synced!
-    while True:
-        props = golos.get_dynamic_global_properties()
-        chain_time = datetime.strptime(props['time'], '%Y-%m-%dT%H:%M:%S')
-        time_diff = datetime.utcnow() - chain_time
-        if time_diff.total_seconds() > 6:
-            log.warning('node out of sync, timediff: {}, waiting...'.format(time_diff.total_seconds()))
-            time.sleep(60)
-        else:
-            break
-
-    # get current miss count to begin with
-    miss_count = functions.get_witness(golos, conf['witness'])['total_missed']
-    log.info('starting monitoring, current miss count: {}'.format(miss_count))
-
-    miss_window_start_time = datetime.now()
-    miss_window = eval(str(conf['miss_window']))
-    prev_key = str()
-
-    while True:
-
-        try:
-            w = functions.get_witness(golos, conf['witness'])
-        except Exception as e:
-            log.error('failed to get witness data: %s', e)
-            time.sleep(63)
-            continue
-        current_miss_count = w['total_missed']
-        miss_diff = current_miss_count - miss_count
-        log.debug('current miss diff: {}'.format(miss_diff))
-
-        # too much misses and witness running at another node and previous key is not ours
-        if (
-            miss_diff > conf['allowed_misses']
-            and w['signing_key'] != conf['witness_pubkey']
-            and prev_key != conf['witness_pubkey']
-        ):
-
-            # switch witness key to us
-            log.info('switching witness key, miss_diff is: {} > {}'.format(miss_diff, conf['allowed_misses']))
-            try:
-                golos.witness_update(conf['witness_pubkey'], w['url'], w['props'], account=conf['witness'])
-            except Exception as e:
-                log.exception('failed to switch key')
-            else:
-                # reset miss_count after switch
-                miss_count = current_miss_count
-
-        now = datetime.now()
-        time_delta = now - miss_window_start_time
-        if time_delta.total_seconds() > miss_window:
-            log.debug('miss_window ended, beginning a new one')
-            # miss_window ended, begin next miss_window
-            miss_window_start_time = datetime.now()
-            # reset miss_count once per miss_window
-            miss_count = current_miss_count
-
-        # remember current signing key as `previous key`
-        prev_key = w['signing_key']
-
-        # try to check miss counter at each round
-        time.sleep(63)
+    return 0
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
